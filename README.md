90b7777ddf7fcda537d646bd932ee232a95dd181
Dan Kantor <dan@ex.fm>
2012-09-07 12:35:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# event-emitter
+
+
+
+
+## Install
+
+
+     npm install event-emitter
+
+## Testing
+
+    git clone 
+    npm install
+    open test/index.html
---
+++ test/event-emitter.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("event-emitter", function(){
+    it("should have at least one test", function(){
+        assert.fail("Write a test!");
+    });
+});
